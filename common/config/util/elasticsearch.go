6c75810b88b6751f8ccea569e4b60f79fedc0616
刘豪贵 <haogui.liu01@mljr.com>
2020-03-25 18:00:38 +0800

    fix

--- common/config/util/elasticsearch.go
+++ common/config/util/elasticsearch.go
@@ -20,12 +20,12 @@ func EsLogInit(appname string, esCfg *ElasticSearch) {
 	esUrl := fmt.Sprintf("%s://%s:%d", esCfg.Protocol, esCfg.Host, esCfg.Port)
 	client, err := elastic.NewClient(elastic.SetURL(esUrl))
 	if err != nil {
-		Error("initLog elastic.NewClient error", err)
+		log.Error("initLog elastic.NewClient error", err)
 	}
 	esIndex := fmt.Sprintf("%s-%s", appname, time.Now().Format("2006-01-02"))
 	hook, err := elogrus.NewElasticHook(client, esCfg.Host, logrus.DebugLevel, esIndex)
 	if err != nil {
-		Error("initLog NewElasticHook error", err)
+		log.Error("initLog NewElasticHook error", err)
 	}
 	log.AddHook(hook)
 }
